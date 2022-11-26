d6aa4a8b3a93be47de61b7c34517943619395893
Nikhil75 <nikhilmengani0707@gmail.com>
2022-11-26 15:18:20 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div className="iplBackground">
+    <h1 className="iplHeading">Super Over League</h1>
+    <div className="iplImages">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
